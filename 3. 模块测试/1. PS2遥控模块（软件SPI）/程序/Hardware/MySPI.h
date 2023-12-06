ab52e0d70f68556110ededdc4cd6d20fc3cfbb86
流年_Ljjj <1529172756@qq.com>
2023-12-07 00:29:44 +0800

    更新注释 整理一下代码

--- 3. 模块测试/1. PS2遥控模块（软件SPI）/程序/Hardware/MySPI.h
+++ 3. 模块测试/1. PS2遥控模块（软件SPI）/程序/Hardware/MySPI.h
@@ -4,10 +4,11 @@
 #include "stm32f10x.h"
 /*------------添加引脚宏定义-------------*/
 #define PS2_PORT		GPIOB
+
 #define PS2_CS_SEL		GPIO_Pin_12		//PB12
 #define PS2_CLK			GPIO_Pin_13		//PB13
-#define PS2_DO_CMD		GPIO_Pin_14		//PB14(MISO)	STM32(MISO) <- 手柄(DO)
-#define PS2_DI_DAT		GPIO_Pin_15		//PB15(MOSI)	STM32(MOSI) -> 手柄(DI)
+#define PS2_DO_CMD		GPIO_Pin_15		//PB15(MOSI)	STM32(MOSI) -> 手柄(DO/CMD、MISO)
+#define PS2_DI_DAT		GPIO_Pin_14		//PB14(MISO)	STM32(MISO) <- 手柄(DI/DAT、MOSI)
 
 void MySPI_Init(void);
 void MySPI_Start(void);
